af23f0e21ee75f2cfe4e4af69a635e85e1fa5ada
Ramanand Tiwari <112823601+Ramanand1101@users.noreply.github.com>
2023-04-19 20:32:34 +0530

    added

---
+++ script.js
@@ -0,0 +1,28 @@
+const hamburger=document.getElementById("hamb");
+const navbar_menu=document.getElementById("navbar-menu");
+
+//Function for Hamburger navbar
+hamburger.addEventListener("click", (e)=>{
+    if(navbar_menu.style.display=="none"){
+        navbar_menu.style.display="block";
+    }else{
+        navbar_menu.style.display="none";
+    }
+})
+
+//Function for Resume to open on the next tab
+function resume(){
+    window.open("./Ramanand-Tiwari-Resume.pdf","_blank")
+  }
+
+// Third-party library to disply GitHub Calender
+GitHubCalendar(".calendar", "Ramanand1101", {
+    responsive: true,
+    global_stats: true,
+    tooltips: true
+  });
+
+  
+    // const name=req.body.Name;
+    // const email=req.body.Email
+    // const message=req.body.Message;
